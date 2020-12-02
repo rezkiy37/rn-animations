ef6adb87c7aecbbaa016f51fa9faab77894d76d4
Michael Kravchenko <rezkiy37@gmail.com>
2020-12-02 22:02:16 +0200

    initilize anim

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,31 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 
+import LottieView from 'lottie-react-native'
 
 
 
 export default (() => {
+
+  const animRef = useRef<LottieView | null>(null)
+
+  const [showAnim, toggleAnim] = useState<boolean>(true)
+
+  useEffect(() => {
+    showAnim && animRef.current?.play()
+  }, [showAnim])
+
   return (
-    <></>
+    <>
+      <LottieView
+        ref={animation => {
+          animRef.current = animation;
+        }}
+        style={{
+          width: 100,
+          height: 100,
+        }}
+        source={require('./src/covid.json')}
+      />
+    </>
   )
 }) as FC
